8b348481dd7f19eeaacf467b8b6f4ae7e0c0b37f
Mikio Hara <mikioh.mikioh@gmail.com>
2016-07-13 16:32:05 +0900

    tcpopt: update package documentation

---
+++ doc.go
@@ -0,0 +1,23 @@
+// Copyright 2016 Mikio Hara. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package tcpopt implements encoding and decoding of TCP-level socket
+// options.
+//
+// Example:
+//
+//	import "github.com/mikioh/tcp"
+//	import "github.com/mikioh/tcpopt"
+//
+//	tc, err := tcp.NewConn(c)
+//	if err != nil {
+//		// error handling
+//	}
+//	if err := tc.SetOption(tcpopt.KeepAlive(true)); err != nil {
+//		// error handling
+//	}
+//	if err := tc.SetOption(tcpopt.KeepAliveProbeCount(3)); err != nil {
+//		// error handling
+//	}
+package tcpopt
--- option.go
+++ option.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package tcpopt implements encoding and decoding of TCP-level socket
-// options.
 package tcpopt
 
 import "time"
